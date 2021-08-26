36c1d740f41aeefcc57aa4c2d0a4c9821e63bad8
fsierra2 <fsierra2@huskers.unl.edu>
2021-08-26 16:55:39 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,10 +1,18 @@
+/**
+ * Name: Felicity Sierra
+ * Date: 8/26/2021
+ * Assignment: Hack 1.0
+ *
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Felicity Sierra\n");
+  printf("Felicity Sierra!!\n");
+
+  printf("My major is Computer Engineering\n");
 
   return 0;
 }
